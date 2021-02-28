17d815fb5eba1ce2316832008560c4f5ebacc004
gurra <g.linnar@gmail.com>
2021-02-28 17:35:49 +0100

    Added stuff

--- src/main/java/com/iths/labb2/Controller.java
+++ src/main/java/com/iths/labb2/Controller.java
@@ -1,35 +1,43 @@
 package com.iths.labb2;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class Controller {
 
-   private SnakeRepository snakeRepository;
+    private SnakeRepository snakeRepository;
 
-   @Autowired
+    @Autowired
     public Controller(SnakeRepository snakeRepository) {
         this.snakeRepository = snakeRepository;
     }
 
-    @GetMapping("/hello")
-    public List<Snake> sayHello(){
-
+    @GetMapping("/Snakes")
+    public List<Snake> listSnakes() {
         return snakeRepository.findAll();
+    }
+
+    @GetMapping("/snake/{id}")
+    public Snake getOne(@PathVariable Integer id) {
+        return snakeRepository.findById(id)
+                .orElseThrow(()->  new ResponseStatusException(HttpStatus.NOT_FOUND,
+                "Id " + id + " not found."));
 
     }
 
-    @GetMapping("/Tjena")
-    public Optional<Snake> sayyoo(){
-        snakeRepository.save(new Snake("Olle","Kobra", '3',"Male"));
+   /* @GetMapping("/addSnakes")
+    public Optional<Snake> addSnake() {
+        snakeRepository.save(new Snake("Olle", "Kobra", '3', "Male"));
 
         return snakeRepository.findById(2);
-    }
+    }*/
 
 
 }
